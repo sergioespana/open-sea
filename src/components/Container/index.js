8fa3583b60577aa41cecbf4abea6487e696565d6
Niels Bik <nielsrowinbik@gmail.com>
2017-09-14 22:18:32 +0200

    Fix linting errors

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,11 +1,9 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import style from './style';
 
-export default class Container extends Component {
-	render({ children, ...props }) {
-		return <div {...props} class={style.container}>{ children }</div>;
-	}
-}
+const Container = ({ children, ...props }) => <div {...props} class={style.container}>{ children }</div>;
+
+export default Container;
 
 // TODO: Define dynamic props (to adjust the components looks through props and having the component apply a different class to itself)
 // TODO: Check for proptypes
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,13 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import Container from '../components/Container';
 
-export default class Home extends Component {
-	render() {
-		return (
-			<div id="main">
-				<Container>
-					<h1>Home</h1>
-					<p>This is the Home component.</p>
-				</Container>
-			</div>
-		);
-	}
-}
+const Home = () => (
+	<div id="main">
+		<Container>
+			<h1>Home</h1>
+			<p>This is the Home component.</p>
+		</Container>
+	</div>
+);
+
+export default Home;
