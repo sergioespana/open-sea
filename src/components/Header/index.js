577cab5717f312cbb4cf7ab767962d4839523d41
Niels Bik <nielsrowinbik@gmail.com>
2017-11-01 10:49:56 +0100

    Temporarily remove projectpicker from Header, add temporary Logout button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,33 @@
 import { h } from 'preact';
 import { Link } from 'react-router-dom';
 import { map } from 'lodash';
+import styled from 'styled-components';
 
 import Wrapper from './components/Wrapper';
 import MenuButton from './components/MenuButton';
 import Title from './components/Title';
-import ProjectSelect from './components/ProjectSelect';
 
-import { Option } from '../Select';
+// import ProjectSelect from './components/ProjectSelect';
+// import { Option } from '../Select';
+// <ProjectSelect value={org} onChange={(value) => push(`/${value}`)}>
+// 	<Option value="" text="See all" />
+// 	{ map(store.organisations, (org, id) => (
+// 		<Option value={id} text={ org.name } />
+// 	)) }
+// </ProjectSelect>
 
-const Header = ({ match: { params: { org } } }, { router: { history: { push } }, mobxStores: { store } }) => (
+const Logout = styled.p`
+	margin: 0;
+	position: absolute;
+	right: 0;
+	margin-right: 20px;
+`;
+
+const Header = (props, { router: { history: { push } }, mobxStores: { store } }) => (
 	<Wrapper>
 		<MenuButton onClick={store.toggleDrawer} />
-		<Title><Link to="/"><strong>SEA</strong> Manager</Link></Title>
-		{ org && (
-			<ProjectSelect value={org} onChange={(value) => push(`/${value}`)}>
-				<Option value="" text="See all" />
-				{ map(store.organisations, (org, id) => (
-					<Option value={id} text={ org.name } />
-				)) }
-			</ProjectSelect>
-		) }
+		<Title><Link to="/">open<strong>SEA</strong></Link></Title>
+		{ store.isAuthed && <Logout><Link to="/logout">logout</Link></Logout> }
 	</Wrapper>
 );
 
