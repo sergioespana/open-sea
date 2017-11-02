e4398a3fecfa86fc7db7c7901a9c8214a9cdaae9
Niels Bik <nielsrowinbik@gmail.com>
2017-11-02 11:26:03 +0100

    Add slim prop to Container

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,10 +2,11 @@ import { h } from 'preact';
 import styled from 'styled-components';
 
 const Container = styled.div`
-	margin: 0 20px;
-
+	margin: ${props => props.slim ? '0 auto' : '0 20px' };
+	max-width: ${props => props.slim ? '640px' : 'none' };
+	
 	@media (min-width: 1025px) {
-		margin: 0 96px;
+		margin: ${props => props.slim ? '0 auto' : '0 96px' };
 	}
 `;
 
