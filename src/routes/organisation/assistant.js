e2d1325c4ee70b3c628fd0d020d7c24d196aa30b
Niels Bik <nielsrowinbik@gmail.com>
2017-11-02 16:30:31 +0100

    Clear assistant page for now

--- src/routes/organisation/assistant.js
+++ src/routes/organisation/assistant.js
@@ -1,13 +1,5 @@
 import { h } from 'preact';
-import { find } from 'lodash';
 
-import Grid from '../../components/Grid';
-import Card, { CardTitle, CardContent } from '../../components/Card';
-
-const Assistant  = ({ match: { params: { org } } }, { mobxStores: { store } }) => (
-	<Grid cols={3}>
-		
-	</Grid>
-);
+const Assistant  = ({ match: { params: { org } } }) => null;
 
 export default Assistant;
