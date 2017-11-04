95559696aad882afa78577b0af6aeb92e0b3d916
Niels Bik <nielsrowinbik@gmail.com>
2017-11-04 16:30:18 +0100

    Move back to old template structure in an attempt to make this build work

--- src/index.js
+++ src/index.js
@@ -1,41 +1,6 @@
 import './style';
 import 'linkstate/polyfill';
+import App from './components/app';
 
-import { h } from 'preact';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider, observer } from 'mobx-react';
-import store from './stores';
-import PrivateRoute from './components/PrivateRoute';
+export default App;
 
-// Components
-import App from './components/App';
-import Header from './components/Header';
-import Snackbar from './components/Snackbar';
-import CircularProgress from './components/CircularProgress';
-
-// Routes
-import Home from './routes';
-import Account from './routes/account';
-import Organisation from './routes/organisation';
-
-const SEAMan = () => (
-	<Provider store={store}>
-		<Router>
-			<App id="app">
-				<Header />
-				{ store.appIsLoading ? (
-					<CircularProgress centerParent />
-				) : (
-					<Switch>
-						<PrivateRoute path="/" exact component={Home} />
-						<Route path="/account" component={Account} />
-						<Route path="/:org" component={Organisation} />
-					</Switch>
-				) }
-				<Snackbar />
-			</App>
-		</Router>
-	</Provider>
-);
-
-export default observer(SEAMan);
