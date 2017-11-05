917a58ea035148850accd01cf0028c19f76e04aa
Niels Bik <nielsrowinbik@gmail.com>
2017-11-05 16:57:49 +0100

    Add Avatar and Switch components

---
+++ src/components/Avatar.js
@@ -0,0 +1,28 @@
+import { h } from 'preact';
+import styled from 'styled-components';
+
+// TODO: Add icons upport
+
+const Avatar = ({ src, children, ...props }) => {
+	if (src) return <img src={src} {...props} />;
+
+	if (typeof children === 'string') {
+		let words = children.split(' ').slice(0, 2),
+			res = words.map((str) => str[0]);
+		return <div {...props}>{ res }</div>;
+	}
+}
+
+export default styled(Avatar)`
+	position: relative;
+	width: 40px;
+	height: 40px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border-radius: 50%;
+	background-color: rgb(185, 185, 185);
+	color: #fff;
+	font-family: inherit;
+	font-size: 16px;
+`;
