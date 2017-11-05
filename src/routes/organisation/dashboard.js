2658f3e055d71eba713af82f2ae20bde321d1268
Niels Bik <nielsrowinbik@gmail.com>
2017-11-05 17:21:37 +0100

    Empty organisation dashboard in preparation of #15, #10, #14 and #11

--- src/routes/organisation/dashboard.js
+++ src/routes/organisation/dashboard.js
@@ -1,40 +1,10 @@
-import { h, Component } from 'preact';
-import { observer } from 'mobx-react';
+import { h } from 'preact';
 import Container from '../../components/Container';
 
-@observer class Overview extends Component {
-	componentWillMount() {
-		let { mobxStores: { OrgStore } } = this.context,
-			{ match: { params: { org } } } = this.props;
-		
-		OrgStore.checkOrgModelPresent(org);
-	}
-	
-	componentWillReceiveProps(nextProps) {
-		let { mobxStores: { OrgStore } } = this.context,
-			oldOrg = this.props.match.params.org,
-			newOrg = nextProps.match.params.org;
-		
-		if (oldOrg !== newOrg) OrgStore.checkOrgModelPresent(newOrg);
-	}
-
-	render = () => {
-		let { match: { params: { org } } } = this.props,
-			{ mobxStores: { OrgStore } } = this.context;
-
-		if (!OrgStore.checkOrgModelPresent(org, false)) return (
-			<Container>
-				<h1>No model placeholder</h1>
-				<p>TODO: Replace with image</p>
-			</Container>
-		);
-
-		return (
-			<Container>
-				<h1>Overview</h1>
-			</Container>
-		);
-	}
-}
+const Overview = () => (
+	<Container>
+		<h1>Overview</h1>
+	</Container>
+);
 
 export default Overview;
