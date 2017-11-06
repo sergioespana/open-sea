5748cf7a5c120555fcb895fd7f4090e873dd0d3b
Niels Bik <nielsrowinbik@gmail.com>
2017-11-06 15:42:01 +0100

    Refactor Snackbar

--- src/components/Snackbar/index.js
+++ src/components/Snackbar/index.js
@@ -6,16 +6,25 @@ import Wrapper from './components/Wrapper';
 import Content from './components/Content';
 import Action from './components/Action';
 
-const Snackbar = (props, { mobxStores: { SnackStore } }) => (
-	<Portal into="body">
-		{ SnackStore.isOpen ? (
-			<Wrapper doClose={SnackStore.snackbar.get('doClose')} onClick={SnackStore.hide}>
-				{ SnackStore.snackbar.get('message') && <Content>{ SnackStore.snackbar.get('message') }</Content> }
-				{ SnackStore.snackbar.get('action') && SnackStore.snackbar.get('actionMessage') && <Action action={SnackStore.snackbar.get('action')}>{ SnackStore.snackbar.get('actionMessage') }</Action> }
-			</Wrapper>
-		) : null }
-	</Portal>
-);
+const Snackbar = (props, { mobxStores: { SnackStore } }) => {
+	let isOpen = SnackStore.isOpen,
+		snackbar = SnackStore.snackbar,
+		doClose = snackbar.get('doClose'),
+		message = snackbar.get('message'),
+		action = snackbar.get('action'),
+		actionMessage = snackbar.get('actionMessage');
+
+	return (
+		<Portal into="body">
+			{ isOpen ? (
+				<Wrapper doClose={doClose} onClick={SnackStore.hide}>
+					{ message && <Content>{ message }</Content> }
+					{ action && actionMessage && <Action action={action}>{ actionMessage }</Action> }
+				</Wrapper>
+			) : null }
+		</Portal>
+	);
+};
 
 export {
 	Content as SnackbarContent,
