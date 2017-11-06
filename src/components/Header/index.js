dc21acad6ba784ec6483ab07ea01b4b694036456
Niels Bik <nielsrowinbik@gmail.com>
2017-11-06 15:42:25 +0100

    Add project select button to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,24 @@
 import { h } from 'preact';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import Avatar from '../Avatar';
 
 import Wrapper from './components/Wrapper';
 import Container from './components/Container';
 import MenuButton from './components/MenuButton';
 import Title from './components/Title';
+import ProjectSelect from './components/ProjectSelect';
 
-const Header = (props, { router: { history: { push } }, mobxStores: { AppStore, AuthStore } }) => {
+const Header = (props, { router: { history: { push } }, mobxStores: { AppStore, AuthStore, OrgStore } }) => {
 	let name = AuthStore.user.get('name');
 	return (
 		<Wrapper>
 			<Container>
 				<MenuButton onClick={AppStore.toggleDrawer} />
 				<Title><Link to="/">open<strong>SEA</strong></Link></Title>
+				<Route path="/organisation/:id" component={ProjectSelect} />
 			</Container>
 			<Container>
-				{ AuthStore.isAuthed && <Link to="/account" style={{ textDecoration: 'none' }}><Avatar src={AuthStore.user.get('avatar')}>{ name ? name.full : 'AC' }</Avatar></Link> }
+				{ AuthStore.isAuthed && <Link to="/account" style={{ textDecoration: 'none' }}><Avatar size={30} src={AuthStore.user.get('avatar')}>{ name ? name.full : 'AC' }</Avatar></Link> }
 			</Container>
 		</Wrapper>
 	);
