05cf6e81f90d5b593516e0d939b5269f6cf2fa08
Niels Bik <nielsrowinbik@gmail.com>
2017-11-08 09:54:45 +0100

    Make Main component responsive and properly handle slim prop

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,11 @@ import styled from 'styled-components';
 
 const Main = styled.main`
 	padding-top: 48px;
-	padding-left: ${props => props.hasDrawer ? 255 : 0 }px;
 	min-height: 100vh;
+	
+	@media (min-width: 601px) {
+		padding-left: ${props => props.hasDrawer ? 255 : 0 }px;
+	}
 `;
 
 export default Main;
