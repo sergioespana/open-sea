09cc97a10534082374d9f3521206e17238676f51
Niels Bik <nielsrowinbik@gmail.com>
2017-11-09 10:17:04 +0100

    Refactor drawer toggling

--- src/stores/app.js
+++ src/stores/app.js
@@ -5,7 +5,20 @@ class AppStore {
 	@observable isLoading = true;
 	@observable drawerIsOpen = false;
 
-	toggleDrawer = () => this.drawerIsOpen = !this.drawerIsOpen;
+	toggleDrawer = () => {
+		if (this.drawerIsOpen) {
+			this.drawerIsOpen = false;
+			setTimeout(() => window.removeEventListener('click', this.handleWindowClick), 195);
+		}
+		else {
+			this.drawerIsOpen = true;
+			setTimeout(() => window.addEventListener('click', this.handleWindowClick), 225);
+		}
+	}
+
+	handleWindowClick = (event) => {
+		this.toggleDrawer();
+	}
 }
 
 const appStore = new AppStore();
