897e9c115a92a53e0d0f6f1f136dbe7b05271d77
Niels Bik <nielsrowinbik@gmail.com>
2017-12-13 20:02:14 +0100

    Listen to network changes (#24) and display a snackbar when offline

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import * as stores from './stores';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider, observer } from 'mobx-react';
+import React, { Component } from 'react';
 import CenterProgress from './components/CenterProgress';
 import Drawer from 'components/Drawer';
 import Dropzone from 'components/Dropzone';
@@ -11,44 +12,64 @@ import Logout from 'routes/logout';
 import New from 'routes/new';
 import Organisation from 'routes/organisation';
 import PrivateRoute from 'components/PrivateRoute';
-import React from 'react';
 import Settings from 'routes/settings';
 import Signup from 'routes/signup';
 import Snackbar from 'components/Snackbar';
 import theme from './theme';
 import ThemeProvider from 'material-styled-components/theme/ThemeProvider';
 
-const App = () => (
-	<Provider {...stores}>
-		<ThemeProvider theme={theme}>
-			<BrowserRouter>
-				<Dropzone id="app">
-					<Header />
-					<Drawer />
+class App extends Component {
 
-					{ stores.AuthStore.loading ? (
-						<CenterProgress />
-					) : (
-						<Switch>
-							<Route path="/login" exact component={Login} />
-							<Route path="/logout" exact component={Logout} />
-							<Route path="/signup" exact component={Signup} />
+	onNetworkStateChanged = () => {
+		const online = navigator.onLine;
+		if (!online) stores.SnackbarStore.show('No internet connection', 0);
+		else stores.SnackbarStore.hide();
+	}
 
-							<Route path="/contact" exact />
-							<Route path="/about" />
+	componentWillMount() {
+		window.addEventListener('online', this.onNetworkStateChanged);
+		window.addEventListener('offline', this.onNetworkStateChanged);
+	}
+	
+	componentWillUnmount() {
+		window.removeEventListener('online', this.onNetworkStateChanged);
+		window.removeEventListener('offline', this.onNetworkStateChanged);
+	}
+	
+	render() {
+		return (
+			<Provider {...stores}>
+				<ThemeProvider theme={theme}>
+					<BrowserRouter>
+						<Dropzone id="app">
+							<Header />
+							<Drawer />
 
-							<PrivateRoute path="/" exact component={Home} />
-							<PrivateRoute path="/new" exact component={New} />
-							<PrivateRoute path="/settings" component={Settings} />
-							<Route path="/:id" component={Organisation} />
-						</Switch>
-					) }
+							{ stores.AuthStore.loading ? (
+								<CenterProgress />
+							) : (
+								<Switch>
+									<Route path="/login" exact component={Login} />
+									<Route path="/logout" exact component={Logout} />
+									<Route path="/signup" exact component={Signup} />
 
-					<Snackbar />
-				</Dropzone>
-			</BrowserRouter>
-		</ThemeProvider>
-	</Provider>
-);
+									<Route path="/contact" exact />
+									<Route path="/about" />
+
+									<PrivateRoute path="/" exact component={Home} />
+									<PrivateRoute path="/new" exact component={New} />
+									<PrivateRoute path="/settings" component={Settings} />
+									<Route path="/:id" component={Organisation} />
+								</Switch>
+							) }
+
+							<Snackbar />
+						</Dropzone>
+					</BrowserRouter>
+				</ThemeProvider>
+			</Provider>
+		);
+	}
+}
 
 export default observer(App);
