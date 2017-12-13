f7c96a2f39aab8d5863d537461b70fc64a720c33
Niels Bik <nielsrowinbik@gmail.com>
2017-12-13 19:50:03 +0100

    Fix props being passed to the DOM object

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,10 +2,6 @@ import container from 'styles/container';
 import { createElement } from 'react';
 import styled from 'styled-components';
 
-const Component = ({ component = 'div', children, ...props }) => createElement(component, { ...props }, children);
-
-const Container = styled(Component)`
-	${container}
-`;
+const Container = styled(({ component = 'div', medium, slim, ...props }) => createElement(component, { ...props })) `${container}`;
 
 export default Container;
