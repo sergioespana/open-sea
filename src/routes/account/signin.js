2b3828ecd9b1d931388eaaf3441cd27d2fc00fe0
Niels Bik <nielsrowinbik@gmail.com>
2018-01-26 17:11:42 +0100

    Send password reset mail if user requests one (SEA-17)

--- src/routes/account/signin.js
+++ src/routes/account/signin.js
@@ -14,7 +14,7 @@ const isBlank = (str) => !trim(str);
 class AccountSignIn extends Component {
 	state = {
 		email: '',
-		error: null,
+		alert: {},
 		password: ''
 	}
 
@@ -23,23 +23,41 @@ class AccountSignIn extends Component {
 		const { AuthStore, VisualStore } = this.props;
 
 		event.preventDefault();
-		this.setState({ error: null });
+		this.setState({ alert: {} });
 		VisualStore.setBusy(true);
 		const { code } = await AuthStore.signIn(email, password);
 		VisualStore.setBusy(false);
+		
 		if (!code) return;
+		return this.handleError(code);
+	}
 
+	resetPassword = async (event) => {
+		const { email } = this.state;
+		const { AuthStore, VisualStore } = this.props;
+
+		this.setState({ alert: {} });
+		VisualStore.setBusy(true);
+		const { code } = await AuthStore.resetPassword(email);
+		VisualStore.setBusy(false);
+
+		if (!code) return this.handleError('auth/reset-sent');
+		return this.handleError(code);
+	}
+
+	handleError = (code) => {
 		switch (code) {
-			case 'auth/invalid-email': return this.setState({ error: 'The provided email address is invalid.' });
-			case 'auth/user-disabled': return this.setState({ error: <span>Your account is currently disabled. Please <Link to="/contact">contact support</Link>.</span> });
-			case 'auth/user-not-found': return this.setState({ error: <span>No account exists for this email address. You may sign up for an account <Link to="/account/signup">here</Link>.</span> });
-			case 'auth/wrong-password': return this.setState({ error: <span>The provided password is incorrect for this account. <Link to="/account/reset-password">Forgot your password?</Link></span> });
-			default: return this.setState({ error: 'An unknown error has occurred.' });
+			case 'auth/reset-sent': return this.setState({ alert: { type: 'info', message: 'A password reset link was sent to your email address.' } });
+			case 'auth/invalid-email': return this.setState({ alert: { type: 'error', message: 'The provided email address is invalid.' } });
+			case 'auth/user-disabled': return this.setState({ alert: { type: 'error', message: <span>Your account is currently disabled. Please <Link to="/contact">contact support</Link>.</span> } });
+			case 'auth/user-not-found': return this.setState({ alert: { type: 'error', message: <span>No account exists for this email address. You may sign up for an account <Link to="/account/signup">here</Link>.</span> } });
+			case 'auth/wrong-password': return this.setState({ alert: { type: 'error', message: <span>The provided password is incorrect for this account. <a onClick={ this.resetPassword }>Forgot your password?</a></span> } });
+			default: return this.setState({ alert: { type: 'error', message: 'An unknown error has occurred.' } });
 		}
 	}
 
 	render = () => {
-		const { email, error, password } = this.state;
+		const { email, alert, password } = this.state;
 		const { state } = this.props;
 		const { busy } = state;
 		const shouldPreventSubmit = isBlank(email) || isBlank(password) || busy;
@@ -50,7 +68,7 @@ class AccountSignIn extends Component {
 					<h1>Welcome back to openSEA</h1>
 				</header>
 				<section>
-					<Alert message={error} type="error" />
+					<Alert message={alert.message} type={alert.type} />
 					<Input
 						type="email"
 						value={email}
