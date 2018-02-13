47f0d16fa10212041ca75bcd4291cda4987fafd9
Niels Bik <nielsrowinbik@gmail.com>
2018-02-13 14:20:31 +0100

    Visual changes to Product pages

--- src/routes/product/index.js
+++ src/routes/product/index.js
@@ -1,17 +1,60 @@
-import { Link, Switch } from 'react-router-dom';
 import React, { Fragment } from 'react';
+import { NavLink } from 'components/Link';
 import Product from './product';
 import ProductFeatures from './features';
 import Route from 'components/Route';
+import styled from 'styled-components';
+import { Switch } from 'react-router-dom';
+
+const Navigation = styled.nav`
+	position: sticky;
+	height: 56px;
+	display: flex;
+	align-items: center;
+`;
+
+const NavigationItem = styled(({ cta, ...props }) => <NavLink {...props} />)`
+	font-size: 1.25rem;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	padding: 0 5px;
+	margin: 0 8px;
+
+	&[aria-current="true"] {
+		font-weight: 500;
+	}
+
+	${({ cta }) => cta && `
+		padding: 3px 8px;
+		height: auto;
+		border: 1px solid #0052CC;
+		border-radius: 3px;
+		font-weight: 500;
+		color: inherit;
+		transition: all 200ms ease;
+
+		:hover {
+			color: #ffffff;
+			background-color: #0052CC;
+			text-decoration: none;
+		}
+	`}
+`;
+
+const NavigationSeparator = styled.div`
+	flex: auto;
+`;
 
 const ProductRoutes = (props) => (
 	<Fragment>
-		<nav>
-			<Link to="/product">Home</Link>
-			<Link to="/product/features">Features</Link>
-			<Link to="/account/signin">Log in</Link>
-			<Link to="/account/signup">Get started</Link>
-		</nav>
+		<Navigation>
+			<NavigationItem to="/product" exact>Home</NavigationItem>
+			<NavigationItem to="/product/features">Features</NavigationItem>
+			<NavigationSeparator />
+			<NavigationItem to="/account/signin">Log in</NavigationItem>
+			<NavigationItem to="/account/signup" cta>Get started</NavigationItem>
+		</Navigation>
 		<Switch>
 			<Route path="/product" exact component={Product} />
 			<Route path="/product/features" exact component={ProductFeatures} />
