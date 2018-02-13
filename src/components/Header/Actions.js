b9141c24270f059903c90424c035f2ae26449c24
Niels Bik <nielsrowinbik@gmail.com>
2018-02-13 13:59:45 +0100

    Add spacing to Header Action items

--- src/components/Header/Actions.js
+++ src/components/Header/Actions.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 const Actions = styled.div`
 	display: flex;
 	align-items: flex-end;
+
+	& > *:not(:last-child) {
+		margin-right: 4px;
+	}
 `;
 
 export default Actions;
