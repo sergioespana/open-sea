23b636e6e56e1f55933276894bd19148e80801b6
Niels Bik <nielsrowinbik@gmail.com>
2018-02-19 11:26:14 +0100

    Visual changes to forms

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const standalone = css`
 	padding: 30px;
-	border: 1px solid #ccc;
-	border-radius: 5px;
+	/* border: 1px solid #ccc; */
+	/* border-radius: 5px; */
 	margin: 60px auto;
 `;
 
@@ -14,41 +14,38 @@ const Form = styled(({ standalone, ...props }) => <form {...props} />)`
 
 	header {
 		padding: 0 0 12px;
-		border-bottom: 1px solid #ccc;
-
+		
 		h1 {
 			margin: 0;
 			font-weight: 500;
 			font-size: 1.5rem;
 		}
-
+		
 		& + section {
 			padding: 20px 0 0;
 		}
 	}
-
+	
 	section {
 		width: 100%;
-
+		
 		& > h1 {
 			font-size: 1rem;
 			font-weight: 600;
-
+			
 			&:first-child {
 				margin-top: 0;
 			}
 		}
-
-		& > p {
-			/* font-size: 0.875rem */;
-		}
 	}
-
+	
 	footer {
 		display: flex;
 		align-items: center;
-		justify-content: ${({ standalone }) => standalone ? 'flex-end' : 'flex-start'};
-		padding: 16px 16px 16px 0;
+		justify-content: 'flex-start';
+		padding: 24px 0;
+		margin-top: 24px;
+		border-top: 1px solid #ccc;
 
 		& > :not(:first-child) {
 			margin-left: 10px;
