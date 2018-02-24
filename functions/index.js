0f35d3d08f798fc970dec45f2f45da049a323052
Niels Bik <nielsrowinbik@gmail.com>
2018-02-24 14:16:54 +0100

    Initialise Cloud Functions and add one to propagage user access

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const admin = require('firebase-admin');
+const camelCase = require('camelcase');
+const functions = require('firebase-functions');
+const glob = require('glob');
+
+admin.initializeApp(functions.config().firebase);
+
+const files = glob.sync('./**/*.f.js', {
+	cwd: __dirname,
+	ignore: './node_modules/**'
+});
+
+files.forEach(file => {
+	let name = camelCase(file.slice(0, -5).split('/').join('_'));
+
+	!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === name
+		? (exports[name] = require(file))
+		: console.error('Something went wrong!');
+});
