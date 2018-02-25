74d657ad69f3afcfcd088c78a6a24d4232ee0c29
Niels Bik <nielsrowinbik@gmail.com>
2018-02-25 21:33:11 +0100

    Remove downloads page since it serves no purpose at the moment

--- src/routes/organisation/index.js
+++ src/routes/organisation/index.js
@@ -4,7 +4,6 @@ import { app } from 'mobx-app';
 import { Link } from 'components/Link';
 import NetworkOrganisations from './network/organisations';
 import NetworkOverview from './network/overview';
-import OrganisationDownloads from './downloads';
 import OrganisationOverview from './overview';
 import OrganisationReportRoutes from './report';
 import OrganisationReports from './reports';
@@ -64,7 +63,6 @@ class OrganisationRoutes extends Component {
 					<Route path="/:orgId" exact component={OrganisationOverview} />
 					<Route path="/:orgId/overview" exact component={OrganisationOverview} />
 					<Route path="/:orgId/reports" exact component={OrganisationReports} />
-					<Route path="/:orgId/downloads" exact component={OrganisationDownloads} />
 					<Route path="/:orgId/settings" component={OrganisationSettings} authedOnly />
 					<Route path="/:orgId/:repId" component={OrganisationReportRoutes} />
 				</Switch>
