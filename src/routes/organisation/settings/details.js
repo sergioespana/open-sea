6d91a88f22f4ffcd4671ecc6089f8ad869d2ef00
Niels Bik <nielsrowinbik@gmail.com>
2018-03-07 16:50:23 +0100

    Add new TextField to organisation detail settings page

--- src/routes/organisation/settings/details.js
+++ src/routes/organisation/settings/details.js
@@ -3,30 +3,34 @@ import { inject, observer } from 'mobx-react';
 import { app } from 'mobx-app';
 import Button from 'components/Button';
 import React from 'react';
+import { TextField } from 'components/Input';
 
 const OrganisationSettingsDetails = inject(app('OrganisationsStore'))(observer((props) => {
 	const { match: { params: { orgId } }, OrganisationsStore } = props;
 	const organisation = OrganisationsStore.getItem(orgId, '_id');
 
 	return (
-		<Form>
-			<section>
+		<section>
+			<Form>
 				<h1>Organisation details</h1>
-				<Input
-					type="image"
-					label="Avatar"
-					value={organisation.avatar}
-				/>
-				<Input
-					label="Name"
-					help="Changes the organisation's name will not be reflected in its ID."
-					value={organisation.name}
-				/>
-			</section>
-			<footer>
-				<Button appearance="primary" type="submit">Save changes</Button>
-			</footer>
-		</Form>
+				<section>
+					<Input
+						type="image"
+						label="Avatar"
+						value={organisation.avatar}
+					/>
+					<TextField
+						label="Name"
+						help="Changes the organisation's name will not be reflected in its ID."
+						defaultValue={organisation.name}
+						compact
+					/>
+				</section>
+				<footer>
+					<Button appearance="primary" type="submit">Save changes</Button>
+				</footer>
+			</Form>
+		</section>
 	);
 }));
 
