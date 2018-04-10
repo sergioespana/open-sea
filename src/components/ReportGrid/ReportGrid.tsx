06417d2202707f392bdf016b0ddca606d55af296
Niels Bik <nielsrowinbik@gmail.com>
2018-04-10 12:18:27 +0200

    Minor changes to ReportGrid components

--- src/components/ReportGrid/ReportGrid.tsx
+++ src/components/ReportGrid/ReportGrid.tsx
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 
 export default styled.div`
 	display: grid;
+	grid-template-columns: auto;
 	width: 100%;
 `;
--- src/components/ReportGrid/ReportGridItem.tsx
+++ src/components/ReportGrid/ReportGridItem.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export default styled.div`
+	width: 100%;
+
 	& > h2 {
 		text-align: center;
 	}
