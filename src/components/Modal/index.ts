a5712cea9c97a817d66d12a77f33de69c2fd399a
Niels Bik <nielsrowinbik@gmail.com>
2018-05-02 16:31:13 +0200

    Turn Modal into a react-loadable component (WEB-90)

--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -1,4 +1,12 @@
-export { default, default as Modal } from './Modal';
+import Loadable from 'react-loadable';
 export { default as ModalFooter } from './ModalFooter';
 export { default as ModalHeader } from './ModalHeader';
 export { default as ModalSection } from './ModalSection';
+
+const Modal = Loadable({
+	loader: () => import('./Modal'),
+	loading: () => null
+});
+
+export { Modal };
+export default Modal;
