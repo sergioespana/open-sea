3bcf012ed6e723ab5d0df90c71d6a053221471bc
Niels Bik <nielsrowinbik@gmail.com>
2018-05-02 16:03:17 +0200

    Add helper functions

--- src/stores/helpers.ts
+++ src/stores/helpers.ts
@@ -1,7 +1,11 @@
-import { find, set } from 'lodash';
+import { find, Many, omitBy, PropertyName, set } from 'lodash';
 import { action } from 'mobx';
 import { User } from '../domain/User';
 
 export const getCurrentUser = (state): User => find(state.users, ['_isCurrent', true]);
 
-export const setAppState = action(set);
+export const setAppState = action((object: any, path: Many < PropertyName >, value: any) => set(object, path, value));
+
+export const omitKeysPrefixedWithChar = (str: string) => (obj: object) => omitBy(obj, (val, key) => key[0] === str);
+
+export const removePrivates = omitKeysPrefixedWithChar('_');
