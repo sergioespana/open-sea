7838cec83f66136825794c1204189b1ae3812752
Niels Bik <nielsrowinbik@gmail.com>
2018-06-13 14:13:18 +0200

    Add ability to select which report a certification assessment is based on (WEB-116)

--- src/routes/organisation/certification.tsx
+++ src/routes/organisation/certification.tsx
@@ -1,110 +1,134 @@
+import linkState from 'linkstate';
 import { filter, get, isUndefined, last, map, partition } from 'lodash';
 import { app } from 'mobx-app';
 import { inject, observer } from 'mobx-react';
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import Container from '../../components/Container';
 import Header from '../../components/Header';
 import Lozenge from '../../components/Lozenge';
 import { Section } from '../../components/Section';
+import Select from '../../components/Select';
 import { Certification, Organisation, Requirement } from '../../domain/Organisation';
+import collection from '../../stores/collection';
 
-const OrganisationCertification = inject(app('OrganisationsStore', 'ReportsStore'))(observer((props) => {
-	const { match: { params: { orgId } }, OrganisationsStore, ReportsStore } = props;
-	const organisation: Organisation = OrganisationsStore.findById(orgId) || {};
-	const network: Organisation = OrganisationsStore.findParentNetworkById(orgId);
-	const withData = filter(organisation._reports, 'data');
-	const report = last(withData);
-	const model = get(report, 'model');
-	const certifications: Certification[] = get(model, 'certifications');
+interface State {
+	baseReport: string;
+}
 
-	const PageHead = (
-		<Header
-			title="Certification"
-			headTitle={`${organisation.name} / Certification`}
-			breadcrumbs={[
-				<Link key={`/${orgId}`} to={`/${orgId}`}>{organisation.name}</Link>
-			]}
-		/>
-	);
+@inject(app('OrganisationsStore', 'ReportsStore'))
+@observer
+class OrganisationCertification extends Component<any, State> {
+	readonly state: State = {
+		baseReport: ''
+	};
 
-	if (!certifications) return <Redirect to={`/${orgId}`} />;
+	componentWillMount () {
+		const { match: { params: { orgId } }, OrganisationsStore } = this.props;
+		const organisation: Organisation = OrganisationsStore.findById(orgId) || {};
+		const withData = filter(organisation._reports, 'data');
+		this.setState({ baseReport: last(withData)._id });
+	}
 
-	const assessed: Certification[] = ReportsStore.assess(certifications, model.indicators, report);
-	const { current: currentIndex, next: nextIndex } = ReportsStore.getCertificationIndex(assessed);
-	const current = assessed[currentIndex];
-	const next = assessed[nextIndex];
+	render () {
+		const { match: { params: { orgId } }, OrganisationsStore, ReportsStore } = this.props;
+		const { baseReport } = this.state;
+		const organisation: Organisation = OrganisationsStore.findById(orgId) || {};
+		const network: Organisation = OrganisationsStore.findParentNetworkById(orgId);
+		const report = collection(organisation._reports).findById(baseReport);
+		const model = get(report, 'model');
+		const certifications: Certification[] = get(model, 'certifications');
 
-	const CurrentCertification = (
-		<div>
-			<h3>Current level</h3>
-			<p>
-				{isUndefined(current)
-					? <span>None</span>
-					: <Lozenge appearance="default" bg={current.colour}>{current.name}</Lozenge>}
-			</p>
-		</div>
-	);
+		const PageHead = (
+			<Header
+				title="Certification"
+				headTitle={`${organisation.name} / Certification`}
+				breadcrumbs={[
+					<Link key={`/${orgId}`} to={`/${orgId}`}>{organisation.name}</Link>
+				]}
+			>
+				<Select
+					isCompact
+					onChange={linkState(this, 'baseReport', 'value')}
+					options={map(organisation._reports, ({ _id, name }) => ({ value: _id, label: name }))}
+					placeholder="Choose report"
+					value={baseReport}
+				/>
+			</Header>
+		);
 
-	if (isUndefined(next)) return (
-		<React.Fragment>
-			{PageHead}
-			<Container>
-				<Section>
-					<p style={{ marginTop: 0 }}>
-						<i>Certification level based on <Link to={`/${report._id}`}>{report.name}</Link>.</i>
-					</p>
-					{CurrentCertification}
-				</Section>
-			</Container>
-		</React.Fragment>
-	);
+		if (!certifications) return <Redirect to={`/${orgId}`} />;
 
-	const [unmet, met] = partition(next.requirements, { _pass: false });
+		const assessed: Certification[] = ReportsStore.assess(certifications, model.indicators, report);
+		const { current: currentIndex, next: nextIndex } = ReportsStore.getCertificationIndex(assessed);
+		const current = assessed[currentIndex];
+		const next = assessed[nextIndex];
 
-	return (
-		<React.Fragment>
-			{PageHead}
-			<Container>
-				<Section>
-					<p style={{ marginTop: 0 }}>
-						<i>Certification level based on <Link to={`/${report._id}`}>{report.name}</Link>.</i>
-					</p>
-					{CurrentCertification}
-					<div>
-						<h3>Next level</h3>
-						<p>
-							<Lozenge appearance="default" bg={next.colour}>{next.name}</Lozenge>
-						</p>
-						<h3>Requirements already met</h3>
-						<ul>
-							{map(met, (requirement: Requirement) => {
-								const { _computed, indicator, operator, value } = requirement;
-								return (
-									<li>
-										<strong>{get(network, `model.indicators.${indicator}.name`)}</strong> ({indicator}) should be {ReportsStore.operatorText[operator]}
-										&nbsp;{value}{operator !== '==' ? `. It currently has a value of ${_computed}.` : ', which it is.'}
-									</li>
-								);
-							})}
-						</ul>
-						<h3>Requirements to meet</h3>
-						<ul>
-							{map(unmet, (requirement: Requirement) => {
-								const { _computed, indicator, operator, value } = requirement;
-								return (
-									<li>
-										<strong>{get(network, `model.indicators.${indicator}.name`)}</strong> ({indicator})
+		const CurrentCertification = (
+			<div>
+				<h3>Current level</h3>
+				<p>
+					{isUndefined(current)
+						? <span>None</span>
+						: <Lozenge appearance="default" bg={current.colour}>{current.name}</Lozenge>}
+				</p>
+			</div>
+		);
+
+		if (isUndefined(next)) return (
+			<React.Fragment>
+				{PageHead}
+				<Container>
+					<Section>
+						{CurrentCertification}
+					</Section>
+				</Container>
+			</React.Fragment>
+		);
+
+		const [unmet, met] = partition(next.requirements, { _pass: false });
+
+		return (
+			<React.Fragment>
+				{PageHead}
+				<Container>
+					<Section>
+						{CurrentCertification}
+						<div>
+							<h3>Next level</h3>
+							<p>
+								<Lozenge appearance="default" bg={next.colour}>{next.name}</Lozenge>
+							</p>
+							<h3>Requirements already met</h3>
+							<ul>
+								{map(met, (requirement: Requirement) => {
+									const { _computed, indicator, operator, value } = requirement;
+									return (
+										<li>
+											<strong>{get(network, `model.indicators.${indicator}.name`)}</strong> ({indicator}) should be {ReportsStore.operatorText[operator]}
+											&nbsp;{value}{operator !== '==' ? `. It currently has a value of ${_computed}.` : ', which it is.'}
+										</li>
+									);
+								})}
+							</ul>
+							<h3>Requirements to meet</h3>
+							<ul>
+								{map(unmet, (requirement: Requirement) => {
+									const { _computed, indicator, operator, value } = requirement;
+									return (
+										<li>
+											<strong>{get(network, `model.indicators.${indicator}.name`)}</strong> ({indicator})
 										should be {ReportsStore.operatorText[operator]} {value}, however it is currently at {_computed}.
 									</li>
-								);
-							})}
-						</ul>
-					</div>
-				</Section>
-			</Container>
-		</React.Fragment>
-	);
-}));
+									);
+								})}
+							</ul>
+						</div>
+					</Section>
+				</Container>
+			</React.Fragment>
+		);
+	}
+}
 
 export default OrganisationCertification;
