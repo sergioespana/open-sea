64424685699bc4b9a8a717c9ebc334807a4b65bb
Niels Bik <nielsrowinbik@gmail.com>
2018-06-13 15:50:22 +0200

    Make progress bar slightly thinner

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -4,17 +4,17 @@ import styled from '../util/styled-components';
 const UnstyledProgress: SFC<HTMLProps<HTMLProgressElement>> = (props) => <progress {...props} />;
 const Progress = styled(UnstyledProgress)`
 	appearance: none;
+	height: 6px;
 	width: 100%;
 
 	&::-webkit-progress-bar {
-		background-color: #eee;
-		border-radius: 3px;
-		box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25) inset;
+		background-color: ${({ theme }) => theme.light};
+		border-radius: 6px;
 	}
 
 	&::-webkit-progress-value {
 		background-color: ${({ theme }) => theme.primary};
-		border-radius: 3px;
+		border-radius: 6px;
 	}
 `;
 
